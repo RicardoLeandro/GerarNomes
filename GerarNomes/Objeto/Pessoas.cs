4fdc60bbaa3a89ba8150b96f73dc9a859aa13c88
RicardoLeandro <ricardoleandrojr9@gmail.com>
2023-05-22 22:07:46 -0300

    Adicionar arquivos de projeto.

---
+++ GerarNomes/Objeto/Pessoas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerarNomes.objeto
+{
+    internal class Pessoas
+    {
+        public string? nome { get; set; }
+        public string? sobrenome { get; set; }
+        public DateTime datanascimento { get; set; }
+        public string? email { get; set; }
+        public string? cpf { get; set; }
+        public string? fone { get; set; }
+        public string? sexo { get; set; }
+        public string? estadocivil { get; set; }
+        public string? Hobbie { get; set; }
+        public int? Idade { get; set; }
+    }
+
+}
